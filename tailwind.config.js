cf3b6ac7564791faf3eb462d6241b5941d5a3683
MarcDufresne <marc.andre.dufresne@gmail.com>
2024-01-26 23:33:23 -0500

    added services and technology sections, various styling improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -7,7 +9,34 @@ module.exports = {
         "./node_modules/flowbite/**/*.js",
     ],
     theme: {
+        colors: {
+            primary: {
+                DEFAULT: "#F37000",
+                50: "#fff9ec",
+                100: "#fff2d3",
+                200: "#ffe2a5",
+                300: "#ffcb6d",
+                400: "#ffa932",
+                500: "#ff8e0a",
+                600: "#f37000",
+                700: "#cc5502",
+                800: "#a1420b",
+                900: "#82380c",
+                950: "#461a04",
+            },
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            slate: colors.slate,
+            emerald: colors.emerald,
+            indigo: colors.indigo,
+            yellow: colors.yellow,
+            orange: colors.orange,
+        },
         extend: {},
+        fontFamily: {
+            sans: ["Inter", "sans-serif"],
+        },
     },
     plugins: [
         require('flowbite/plugin'),
